93a3567267b86696728f8bd459131d96796fa7df
pasuunapaholainen <tirkkosolli@gmail.com>
2024-03-13 15:33:57 +0200

    Lemmikkiluokan luonti ja suurin osa metodeista

---
+++ src/main/java/com/example/lemmikkipeli/Lemmikki.java
@@ -0,0 +1,69 @@
+package com.example.lemmikkipeli;
+
+import java.io.Serializable;
+
+public class Lemmikki implements Serializable {
+
+    private String laji;
+    private String nimi;
+    private long viimeSyotto;
+    private long viimeLeikki;
+
+    public Lemmikki(){
+        this.laji = "";
+        this.nimi = "";
+    }
+
+    public Lemmikki(String laji, String nimi){
+        this.laji = laji;
+        this.nimi = nimi;
+    }
+
+    public void setLaji(String laji){
+        this.laji = laji;
+    }
+
+    public void setNimi(String nimi){
+        this.nimi = nimi;
+    }
+
+    public void setViimeSyotto(){
+        this.viimeSyotto = System.currentTimeMillis();
+    }
+
+    public void setViimeLeikki(){
+        this.viimeLeikki = System.currentTimeMillis();
+    }
+
+    public String getLaji(){
+        return laji;
+    }
+
+    public String getNimi(){
+        return nimi;
+    }
+
+    public long getViimeSyotto(){
+        return viimeSyotto;
+    }
+
+    public long getViimeLeikki(){
+        return viimeLeikki;
+    }
+
+    public boolean pitkaAikaSyotosta(){
+        if(viimeSyotto < (getViimeSyotto()-10800000/* 3h */)){
+            return true;
+        } else {
+           return false;
+        }
+    }
+
+    public boolean pitkaAikaLeikista(){
+        if (viimeLeikki < (getViimeLeikki()-5400000/* 1.5 h */)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
